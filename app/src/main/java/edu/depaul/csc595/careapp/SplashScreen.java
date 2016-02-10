b37e747cce13af3a17164683276dcb8229c590ba
jeffersonalvess <asilva.jefferson@gmail.com>
2016-02-10 15:04:18 -0600

    Splashscreen

---
+++ app/src/main/java/edu/depaul/csc595/careapp/SplashScreen.java
@@ -0,0 +1,31 @@
+package edu.depaul.csc595.careapp;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+    }
+}

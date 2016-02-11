0a9f206ba46f0bbb190541cafb8359335686d372
Renato <renato.leonel93@gmail.com>
2016-02-10 20:52:27 -0600

    Aeee

--- app/src/main/java/edu/depaul/csc595/careapp/MainActivity.java
+++ app/src/main/java/edu/depaul/csc595/careapp/MainActivity.java
@@ -48,7 +48,7 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                //Jefferson Gordo
             }
         });
 

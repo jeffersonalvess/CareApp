f90ffa23554396cc7715c50295a3ab357528be8e
matheuscz <mahecaza@hotmail.com>
2016-02-15 15:51:31 -0600

    Facebook login

--- app/src/main/java/edu/depaul/csc595/careapp/SplashScreen.java
+++ app/src/main/java/edu/depaul/csc595/careapp/SplashScreen.java
@@ -1,12 +1,12 @@
 package edu.depaul.csc595.careapp;
 
-import android.annotation.SuppressLint;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
+
+import com.facebook.CallbackManager;
+import com.facebook.login.widget.LoginButton;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -14,9 +14,36 @@ import android.view.View;
  */
 public class SplashScreen extends AppCompatActivity {
 
+    private LoginButton loginButton;
+    public CallbackManager mCallbackManager;
+    public static final String FACEBOOK_PREFS = "FacebookPreferences";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //FacebookSdk.sdkInitialize(getApplicationContext());
+        //mCallbackManager = CallbackManager.Factory.create();
+
+        SharedPreferences settings = getSharedPreferences(FACEBOOK_PREFS, 0);
+
+        if (settings.getBoolean("loginSucessful", true)) {
+            Intent intent = new Intent();
+            intent.setClass(this, TabbedScreen.class);
+            //intent.setClass(WelcomeScreen.this, GoogleMapsTest.class);
+            intent.putExtra("REQ", 1);
+            startActivity(intent);
+            finish();
+        }
+        else
+        {
+            Intent intent = new Intent();
+            intent.setClass(this, MainActivity.class);
+            //intent.setClass(WelcomeScreen.this, GoogleMapsTest.class);
+            intent.putExtra("REQ", 1);
+            startActivity(intent);
+            finish();
+        }
 
         setContentView(R.layout.activity_splash_screen);
         getSupportActionBar().hide();

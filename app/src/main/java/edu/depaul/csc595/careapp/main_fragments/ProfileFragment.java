6c504907f7bb5398224e9edaba214a1e6d47319d
Miller Horvath <miller.horvath@hotmail.com>
2016-02-17 15:37:17 -0600

    Alinhamento dos botões inferiores e titulo da activitys.

--- app/src/main/java/edu/depaul/csc595/careapp/main_fragments/ProfileFragment.java
+++ app/src/main/java/edu/depaul/csc595/careapp/main_fragments/ProfileFragment.java
@@ -1,47 +1,18 @@
 package edu.depaul.csc595.careapp.main_fragments;
 
 
-import android.app.ListActivity;
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.BaseAdapter;
-import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.facebook.AccessToken;
-import com.facebook.GraphRequest;
-import com.facebook.GraphResponse;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
-import de.hdodenhof.circleimageview.CircleImageView;
-import edu.depaul.csc595.careapp.ListData.Card;
-import edu.depaul.csc595.careapp.ListData.CardList;
 import edu.depaul.csc595.careapp.ListData.ProfileList;
 import edu.depaul.csc595.careapp.R;
-import edu.depaul.csc595.careapp.TestActivity;
 
 public class ProfileFragment extends Fragment {
 

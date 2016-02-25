60b78b7809ab012376d007b44609c61eb95215ad
jeffersonalvess <asilva.jefferson@gmail.com>
2016-02-25 17:30:48 -0600

    Commited by @jeffersonalvess
    - Vei fiz um monte de coisa, na moral. Faço ideia do que fiz aqui.

--- app/src/main/java/edu/depaul/csc595/careapp/RideActivity.java
+++ app/src/main/java/edu/depaul/csc595/careapp/RideActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import edu.depaul.csc595.careapp.Design.*;
 
@@ -14,8 +15,23 @@ public class RideActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ride);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if (item.getItemId() == android.R.id.home)
+        {
+            finish();
+        }
+
+        return true;
+    }
+
+
 
 }

3802325b6bfaa6e9a742be3d1183d06db61159fa
jeffersonalvess <asilva.jefferson@gmail.com>
2016-03-02 15:37:57 -0600

    Commited by @jeffersonalvess
    - Achievements Activity

---
+++ app/src/main/java/edu/depaul/csc595/careapp/Helpers/UserProfileAndStatistics.java
@@ -0,0 +1,14 @@
+package edu.depaul.csc595.careapp.Helpers;
+
+/**
+ * Created by jeffersonalvess on 3/2/16.
+ */
+public class UserProfileAndStatistics
+{
+    FacebookUserInfo userProfile;
+    String averageSpeed;
+    String averageInfractionsPerTrip;
+    String averageInfractions100mi;
+    String averageInfractions24h;
+
+}
--- app/src/main/java/edu/depaul/csc595/careapp/RideActivity.java
+++ app/src/main/java/edu/depaul/csc595/careapp/RideActivity.java
@@ -1,20 +1,24 @@
 package edu.depaul.csc595.careapp;
 
-import android.app.Activity;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Space;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import edu.depaul.csc595.careapp.Design.*;
 
 public class RideActivity extends AppCompatActivity {
+
+    private View mProfile;
+    private View mProgress;
+    private int mLongAnimationDuration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +36,15 @@ public class RideActivity extends AppCompatActivity {
         findViewById(R.id.space_profile_1).setVisibility(View.GONE);
         findViewById(R.id.space_profile_2).setVisibility(View.GONE);
         findViewById(R.id.space_profile_3).setVisibility(View.GONE);
+
+
+        mProfile = (LinearLayout) findViewById(R.id.ride_profile_view);
+        mProgress = (LinearLayout) findViewById(R.id.ride_profile_wait);
+
+        mProgress.setVisibility(View.GONE);
+
+        mLongAnimationDuration = getResources().getInteger(android.R.integer.config_longAnimTime);
+
 
         // Tinting icons and setting TextViews text color based on the progress bars value.
         tempCircularProgressBar = (CircularProgressBar) findViewById(R.id.ride_cpBar1);
@@ -69,16 +82,16 @@ public class RideActivity extends AppCompatActivity {
         fabAccept.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                setResult(Activity.RESULT_OK);
-                finish();
+                crossfadeA();
+
+                crossfadeB();
             }
         });
 
         fabRefuse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                setResult(Activity.RESULT_CANCELED);
-                finish();
+                crossfadeA();
             }
         });
 
@@ -99,6 +112,67 @@ public class RideActivity extends AppCompatActivity {
         }
 
         return true;
+    }
+
+    private void loadNewUser(int index)
+    {
+
+    }
+
+    private void crossfadeA() {
+
+        // Set the content view to 0% opacity but visible, so that it is visible
+        // (but fully transparent) during the animation.
+        mProgress.setAlpha(0f);
+        mProgress.setVisibility(View.VISIBLE);
+
+        // Animate the content view to 100% opacity, and clear any animation
+        // listener set on the view.
+        mProgress.animate()
+                .alpha(1f)
+                .setDuration(mLongAnimationDuration)
+                .setListener(null);
+
+        // Animate the loading view to 0% opacity. After the animation ends,
+        // set its visibility to GONE as an optimization step (it won't
+        // participate in layout passes, etc.)
+        mProfile.animate()
+                .alpha(0f)
+                .setDuration(mLongAnimationDuration)
+                .setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        mProfile.setVisibility(View.GONE);
+                    }
+                });
+    }
+
+    private void crossfadeB() {
+
+        // Set the content view to 0% opacity but visible, so that it is visible
+        // (but fully transparent) during the animation.
+        mProfile.setAlpha(0f);
+        mProfile.setVisibility(View.VISIBLE);
+
+        // Animate the content view to 100% opacity, and clear any animation
+        // listener set on the view.
+        mProfile.animate()
+                .alpha(1f)
+                .setDuration(mLongAnimationDuration)
+                .setListener(null);
+
+        // Animate the loading view to 0% opacity. After the animation ends,
+        // set its visibility to GONE as an optimization step (it won't
+        // participate in layout passes, etc.)
+        mProgress.animate()
+                .alpha(0f)
+                .setDuration(mLongAnimationDuration)
+                .setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        mProgress.setVisibility(View.GONE);
+                    }
+                });
     }
 
 

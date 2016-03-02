14aa77290ef5bf994eadbf27d8250b1c9e1a83c8
jeffersonalvess <asilva.jefferson@gmail.com>
2016-03-02 16:55:46 -0600

    Commited by @jeffersonalvess
    - Achievements Activity

--- app/src/main/java/edu/depaul/csc595/careapp/Helpers/UserProfileAndStatistics.java
+++ app/src/main/java/edu/depaul/csc595/careapp/Helpers/UserProfileAndStatistics.java
@@ -1,14 +1,177 @@
 package edu.depaul.csc595.careapp.Helpers;
 
+import android.graphics.Bitmap;
+
+import java.util.concurrent.ExecutionException;
+
 /**
  * Created by jeffersonalvess on 3/2/16.
  */
 public class UserProfileAndStatistics
 {
-    FacebookUserInfo userProfile;
+    //TODO: Fix this class to support real values and the main achievements.
+
+    String fbID;
+    String fbName;
+    Bitmap fbPicture;
+
     String averageSpeed;
     String averageInfractionsPerTrip;
     String averageInfractions100mi;
     String averageInfractions24h;
 
+    String points;
+    String friendChallenges;
+    String achievements;
+    String drivenMiles;
+    String drivenTime;
+    String quiz;
+
+    public UserProfileAndStatistics(String fbID, String fbName, String averageSpeed, String averageInfractionsPerTrip, String averageInfractions100mi, String averageInfractions24h, String points, String friendChallenges, String achievements, String drivenMiles, String drivenTime, String quiz) throws ExecutionException, InterruptedException
+    {
+
+        fbPicture = new FacebookImageLoadTask(fbID).execute().get();
+        this.fbID = fbID;
+        this.fbName = fbName;
+        this.averageSpeed = averageSpeed;
+        this.averageInfractionsPerTrip = averageInfractionsPerTrip;
+        this.averageInfractions100mi = averageInfractions100mi;
+        this.averageInfractions24h = averageInfractions24h;
+        this.points = points;
+        this.friendChallenges = friendChallenges;
+        this.achievements = achievements;
+        this.drivenMiles = drivenMiles;
+        this.drivenTime = drivenTime;
+        this.quiz = quiz;
+    }
+
+    public String getFbID()
+    {
+        return fbID;
+    }
+
+    public void setFbID(String fbID)
+    {
+        this.fbID = fbID;
+    }
+
+    public String getFbName()
+    {
+        return fbName;
+    }
+
+    public void setFbName(String fbName)
+    {
+        this.fbName = fbName;
+    }
+
+    public Bitmap getFbPicture()
+    {
+        return fbPicture;
+    }
+
+    public void setFbPicture(Bitmap fbPicture)
+    {
+        this.fbPicture = fbPicture;
+    }
+
+    public String getAverageSpeed()
+    {
+        return averageSpeed;
+    }
+
+    public void setAverageSpeed(String averageSpeed)
+    {
+        this.averageSpeed = averageSpeed;
+    }
+
+    public String getAverageInfractionsPerTrip()
+    {
+        return averageInfractionsPerTrip;
+    }
+
+    public void setAverageInfractionsPerTrip(String averageInfractionsPerTrip)
+    {
+        this.averageInfractionsPerTrip = averageInfractionsPerTrip;
+    }
+
+    public String getAverageInfractions100mi()
+    {
+        return averageInfractions100mi;
+    }
+
+    public void setAverageInfractions100mi(String averageInfractions100mi)
+    {
+        this.averageInfractions100mi = averageInfractions100mi;
+    }
+
+    public String getAverageInfractions24h()
+    {
+        return averageInfractions24h;
+    }
+
+    public void setAverageInfractions24h(String averageInfractions24h)
+    {
+        this.averageInfractions24h = averageInfractions24h;
+    }
+
+    public String getPoints()
+    {
+        return points;
+    }
+
+    public void setPoints(String points)
+    {
+        this.points = points;
+    }
+
+    public String getFriendChallenges()
+    {
+        return friendChallenges;
+    }
+
+    public void setFriendChallenges(String friendChallenges)
+    {
+        this.friendChallenges = friendChallenges;
+    }
+
+    public String getAchievements()
+    {
+        return achievements;
+    }
+
+    public void setAchievements(String achievements)
+    {
+        this.achievements = achievements;
+    }
+
+    public String getDrivenMiles()
+    {
+        return drivenMiles;
+    }
+
+    public void setDrivenMiles(String drivenMiles)
+    {
+        this.drivenMiles = drivenMiles;
+    }
+
+    public String getDrivenTime()
+    {
+        return drivenTime;
+    }
+
+    public void setDrivenTime(String drivenTime)
+    {
+        this.drivenTime = drivenTime;
+    }
+
+    public String getQuiz()
+    {
+        return quiz;
+    }
+
+    public void setQuiz(String quiz)
+    {
+        this.quiz = quiz;
+    }
 }
--- app/src/main/java/edu/depaul/csc595/careapp/RideActivity.java
+++ app/src/main/java/edu/depaul/csc595/careapp/RideActivity.java
@@ -5,20 +5,32 @@ import android.animation.AnimatorListenerAdapter;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 import edu.depaul.csc595.careapp.Design.*;
+import edu.depaul.csc595.careapp.Helpers.FacebookUserInfo;
+import edu.depaul.csc595.careapp.Helpers.UserProfileAndStatistics;
 
 public class RideActivity extends AppCompatActivity {
 
     private View mProfile;
     private View mProgress;
     private int mLongAnimationDuration;
+    private ArrayList<UserProfileAndStatistics> users;
+    private int indexCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,14 +41,9 @@ public class RideActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        CircularProgressBar tempCircularProgressBar;
-        OurImageView tempOurImageView;
-        TextView tempTextView;
-
         findViewById(R.id.space_profile_1).setVisibility(View.GONE);
         findViewById(R.id.space_profile_2).setVisibility(View.GONE);
         findViewById(R.id.space_profile_3).setVisibility(View.GONE);
-
 
         mProfile = (LinearLayout) findViewById(R.id.ride_profile_view);
         mProgress = (LinearLayout) findViewById(R.id.ride_profile_wait);
@@ -45,53 +52,38 @@ public class RideActivity extends AppCompatActivity {
 
         mLongAnimationDuration = getResources().getInteger(android.R.integer.config_longAnimTime);
 
-
-        // Tinting icons and setting TextViews text color based on the progress bars value.
-        tempCircularProgressBar = (CircularProgressBar) findViewById(R.id.ride_cpBar1);
-        tempOurImageView = (OurImageView) findViewById(R.id.ride_icon1);
-        tempCircularProgressBar.setProgress(85.0f);
-        tempOurImageView.setColorFilter(tempCircularProgressBar.getBackgroundColor());
-        tempTextView = (TextView) findViewById(R.id.ride_num1);
-        tempTextView.setTextColor(tempCircularProgressBar.getBackgroundColor());
-
-        tempCircularProgressBar = (CircularProgressBar) findViewById(R.id.ride_cpBar2);
-        tempOurImageView = (OurImageView) findViewById(R.id.ride_icon2);
-        tempCircularProgressBar.setProgress(20.0f);
-        tempOurImageView.setColorFilter(tempCircularProgressBar.getBackgroundColor());
-        tempTextView = (TextView) findViewById(R.id.ride_num2);
-        tempTextView.setTextColor(tempCircularProgressBar.getBackgroundColor());
-
-        tempCircularProgressBar = (CircularProgressBar) findViewById(R.id.ride_cpBar3);
-        tempOurImageView = (OurImageView) findViewById(R.id.ride_icon3);
-        tempCircularProgressBar.setProgress(60.0f);
-        tempOurImageView.setColorFilter(tempCircularProgressBar.getBackgroundColor());
-        tempTextView = (TextView) findViewById(R.id.ride_num3);
-        tempTextView.setTextColor(tempCircularProgressBar.getBackgroundColor());
-
-        tempCircularProgressBar = (CircularProgressBar) findViewById(R.id.ride_cpBar4);
-        tempOurImageView = (OurImageView) findViewById(R.id.ride_icon4);
-        tempCircularProgressBar.setProgress(30.0f);
-        tempOurImageView.setColorFilter(tempCircularProgressBar.getBackgroundColor());
-        tempTextView = (TextView) findViewById(R.id.ride_num4);
-        tempTextView.setTextColor(tempCircularProgressBar.getBackgroundColor());
-
         FloatingActionButton fabRefuse = (FloatingActionButton) findViewById(R.id.ride_btRefuse);
         FloatingActionButton fabAccept = (FloatingActionButton) findViewById(R.id.ride_btAccept);
         FloatingActionButton fabInfo = (FloatingActionButton) findViewById(R.id.ride_btInfo);
 
+
+        try
+        {
+            createGambiarraList();
+            loadNewUser(indexCount);
+        }
+        catch (ExecutionException e)
+        {
+            e.printStackTrace();
+        }
+        catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+
         fabAccept.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                crossfadeA();
-
-                crossfadeB();
+                loadNewUser(indexCount);
+                Toast.makeText(getApplicationContext(), "You would take a ride with this driver :)", Snackbar.LENGTH_SHORT).show();
             }
         });
 
         fabRefuse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                crossfadeA();
+                loadNewUser(indexCount);
+                Toast.makeText(getApplicationContext(), "You would not take a ride with this driver :(", Snackbar.LENGTH_SHORT).show();
             }
         });
 
@@ -116,7 +108,138 @@ public class RideActivity extends AppCompatActivity {
 
     private void loadNewUser(int index)
     {
+        crossfadeA();
 
+        CircularProgressBar tempCircularProgressBar;
+        OurImageView tempOurImageView;
+        TextView tempTextView;
+
+        UserProfileAndStatistics u = users.get(index);
+
+        tempCircularProgressBar = (CircularProgressBar) findViewById(R.id.ride_cpBar1);
+        tempOurImageView = (OurImageView) findViewById(R.id.ride_icon1);
+        tempCircularProgressBar.setProgress(Integer.parseInt(u.getAverageSpeed()));
+        tempOurImageView.setColorFilter(tempCircularProgressBar.getBackgroundColor());
+        tempTextView = (TextView) findViewById(R.id.ride_num1);
+        tempTextView.setTextColor(tempCircularProgressBar.getBackgroundColor());
+
+        tempCircularProgressBar = (CircularProgressBar) findViewById(R.id.ride_cpBar2);
+        tempOurImageView = (OurImageView) findViewById(R.id.ride_icon2);
+        tempCircularProgressBar.setProgress(Integer.parseInt(u.getAverageInfractionsPerTrip()));
+        tempOurImageView.setColorFilter(tempCircularProgressBar.getBackgroundColor());
+        tempTextView = (TextView) findViewById(R.id.ride_num2);
+        tempTextView.setTextColor(tempCircularProgressBar.getBackgroundColor());
+
+        tempCircularProgressBar = (CircularProgressBar) findViewById(R.id.ride_cpBar3);
+        tempOurImageView = (OurImageView) findViewById(R.id.ride_icon3);
+        tempCircularProgressBar.setProgress(Integer.parseInt(u.getAverageInfractions100mi()));
+        tempOurImageView.setColorFilter(tempCircularProgressBar.getBackgroundColor());
+        tempTextView = (TextView) findViewById(R.id.ride_num3);
+        tempTextView.setTextColor(tempCircularProgressBar.getBackgroundColor());
+
+        tempCircularProgressBar = (CircularProgressBar) findViewById(R.id.ride_cpBar4);
+        tempOurImageView = (OurImageView) findViewById(R.id.ride_icon4);
+        tempCircularProgressBar.setProgress(Integer.parseInt(u.getAverageInfractions24h()));
+        tempOurImageView.setColorFilter(tempCircularProgressBar.getBackgroundColor());
+        tempTextView = (TextView) findViewById(R.id.ride_num4);
+        tempTextView.setTextColor(tempCircularProgressBar.getBackgroundColor());
+
+        TextView a = (TextView) findViewById(R.id.ride_points);
+        TextView b = (TextView) findViewById(R.id.ride_dist_driven);
+        TextView c = (TextView) findViewById(R.id.ride_kd_challenge);
+        TextView d = (TextView) findViewById(R.id.ride_hours_driven);
+        TextView e = (TextView) findViewById(R.id.ride_challenges);
+        TextView f = (TextView) findViewById(R.id.ride_quizes_stats);
+        TextView g = (TextView) findViewById(R.id.profileName);
+        TextView h = (TextView) findViewById(R.id.ride_num1);
+        TextView i = (TextView) findViewById(R.id.ride_num2);
+        TextView j = (TextView) findViewById(R.id.ride_num3);
+        TextView k = (TextView) findViewById(R.id.ride_num4);
+
+        CircleImageView l = (CircleImageView) findViewById(R.id.userProfilePicture);
+
+        a.setText(u.getPoints());
+        b.setText(u.getDrivenMiles());
+        c.setText(u.getFriendChallenges());
+        d.setText(u.getDrivenTime());
+        e.setText(u.getAchievements());
+        f.setText(u.getQuiz());
+        g.setText(u.getFbName());
+        h.setText(u.getAverageSpeed());
+        i.setText(u.getAverageInfractionsPerTrip());
+        j.setText(u.getAverageInfractions100mi());
+        k.setText(u.getAverageInfractions24h());
+        l.setImageBitmap(u.getFbPicture());
+
+        indexCount = (indexCount <= 0) ? users.size() - 1 : indexCount - 1;
+        crossfadeB();
+    }
+
+    private void createGambiarraList() throws ExecutionException, InterruptedException
+    {
+        users = new ArrayList<>();
+
+        users.add(new UserProfileAndStatistics(
+                "10154650092913009",
+                "Lucas",
+                "30",
+                "2",
+                "3",
+                "4",
+                "23000",
+                "975/0",
+                "3",
+                "200 miles",
+                "0.5 hours",
+                "78%"
+        ));
+
+        users.add(new UserProfileAndStatistics(
+                "988159154611919",
+                "Matheus",
+                "60",
+                "7",
+                "2",
+                "1",
+                "27000",
+                "500/0",
+                "10",
+                "150 miles",
+                "2 hours",
+                "90%"
+        ));
+
+        users.add(new UserProfileAndStatistics(
+                "903567903084504",
+                "Renato",
+                "90",
+                "5",
+                "10",
+                "2",
+                "2300",
+                "50/0",
+                "2",
+                "350 miles",
+                "10 hours",
+                "56%"
+        ));
+
+        users.add(new UserProfileAndStatistics(
+                "970064856419280",
+                "Miller",
+                "100",
+                "7",
+                "10",
+                "9",
+                "100",
+                "10/0",
+                "10",
+                "500 miles",
+                "20 hours",
+                "100%"
+        ));
+
+        indexCount = users.size() - 1;
     }
 
     private void crossfadeA() {
